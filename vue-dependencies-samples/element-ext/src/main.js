76d504b0924b35e054ab2f891d8b8649fb5d3070
chenzw <656469722@qq.com>
2020-09-25 15:01:23 +0800

    update

--- vue-dependencies-samples/element-ext/src/main.js
+++ vue-dependencies-samples/element-ext/src/main.js
@@ -5,22 +5,7 @@ import App from './App.vue'
 
 Vue.use(ElementUI)
 
-//import Highlight from './assets/highlight';
-
-// 引入全部语言
-import hljs from 'highlight.js';
-
-// 自定义选择语言
-//import hljs from 'highlight.js/lib/core';
-import 'highlight.js/styles/tomorrow-night.css';
-//import 'highlight.js/styles/github.css';
-
-// 引入插件
-Vue.use(hljs.vuePlugin);
-// 将hljs注册为全局对象
-window.hljs = hljs;
-
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
