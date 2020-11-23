51833de259341debe09b7b36f4da442a3829de56
chenzw <656469722@qq.com>
2020-11-23 14:50:42 +0800

    update

---
+++ vue-dependencies-samples/editor.md/src/components/EditorMd/index.vue
@@ -0,0 +1,45 @@
+<template>
+   <!-- <div
+            :id="id"
+            :editorConfig="editorConfig"
+    >
+
+    </div>-->
+
+    <div id="editorMd">
+
+    </div>
+</template>
+
+<script>
+    import scriptjs from 'scriptjs';
+    import editorMd from 'editor.md/editormd'
+
+    export default {
+        name: "EditorMd",
+        props: {
+            id: {
+                type: String,
+                default: 'editor-md'
+            },
+            editorConfig: {
+                type: Object,
+                default: () => {
+                    return {
+
+                    }
+                }
+            }
+        },
+        mounted() {
+            editorMd("editorMd", {
+                width: "100%",
+                height: "100%"
+            })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
