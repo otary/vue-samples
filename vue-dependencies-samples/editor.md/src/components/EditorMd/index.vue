f2a4e2799ef7555816b80235e7a5fd94972551a6
chenzw <656469722@qq.com>
2020-11-24 10:35:32 +0800

    update

--- vue-dependencies-samples/editor.md/src/components/EditorMd/index.vue
+++ vue-dependencies-samples/editor.md/src/components/EditorMd/index.vue
@@ -6,14 +6,19 @@
 
     </div>-->
 
+
     <div id="editorMd" style="width: 100%; height: 100%;">
+
+        <link
+                href="static/editor.md/css/editormd.css"
+                rel="stylesheet"
+        />
 
     </div>
 </template>
 
 <script>
-    // import scriptjs from 'scriptjs';
-    import editorMd2 from 'editor.md/editormd'
+    import scriptjs from 'scriptjs';
 
     export default {
         name: "EditorMd",
@@ -32,15 +37,44 @@
             }
         },
         mounted() {
-            this.$nextTick(()=> {
-             editorMd2()("editorMd", {
-                    width: '100%',
-                    height: '100%',
-                    path: 'static/editor.md/lib/'
+            scriptjs([
+                `static/jquery/jquery-3.5.1.min.js`,
+                `static/prism/prism.js`,
+                `static/editor.md/lib/raphael.min.js`,
+                `static/editor.md/lib/flowchart.min.js`,
+            ], ()=> {
+                scriptjs([
+                    `static/xss/xss.min.js`,
+                    `static/editor.md/lib/marked.min.js`,
+                    `static/editor.md/lib/prettify.min.js`,
+                    `static/editor.md/lib/underscore.min.js`,
+                    `static/editor.md/lib/sequence-diagram.min.js`,
+                    `static/editor.md/lib/jquery.flowchart.min.js`,
+                ], ()=> {
+                    scriptjs(`static/editor.md/editormd.js`, () => {
+                        this.initEditor()
+                    })
                 })
+            })
 
-            });
+        },
+        methods: {
+            initEditor() {
+                this.$nextTick((editorMD = window.editormd) => {
+                    if (editorMD) {
+                        /*if (this.type == 'editor') {
+                            this.instance = editorMD(this.id, this.editorConfig)
+                        } else {
+                            this.instance = editorMD.markdownToHTML(this.id, this.editorConfig)
+                        }*/
 
+                        editorMD('editorMd', {
+                            path: 'static/editor.md/lib/',
+                        });
+
+                    }
+                })
+            }
         }
     }
 </script>
---
+++ vue-dependencies-samples/flexible/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="app">
+        <div class="left">
+              left
+        </div>
+        <div class="right">
+            right
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        mounted() {
+
+        },
+        methods: {
+            startHacking() {
+                this.$notify({
+                    title: 'It works!',
+                    type: 'success',
+                    message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+                    duration: 5000
+                })
+            }
+        }
+    }
+</script>
+
+<style>
+    #app {
+        width: 1820px;
+        height: 100%;
+        border: 1px solid red;
+    }
+
+    .left {
+        float: left;
+        width: 500px;
+        box-sizing: border-box;
+
+        border: 1px solid #00ff00;
+    }
+
+    .right {
+        float: right;
+        width: calc(100% - 500px);
+
+        box-sizing: border-box;
+        border: 1px solid #3a8ee6;
+    }
+
+    html, body {
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+    }
+
+</style>
