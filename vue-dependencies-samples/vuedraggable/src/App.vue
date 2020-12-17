7f328f2016b4427fb661ec317219d054f2ab6f66
chenzw <656469722@qq.com>
2020-12-17 17:35:12 +0800

    update

---
+++ vue-dependencies-samples/vuedraggable/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="app">
+        <el-card title="简单">
+            <draggable>
+                <transition-group>
+                    <el-button v-for="item in originItems">
+                        {{ item.name }}
+                    </el-button>
+                </transition-group>
+            </draggable>
+        </el-card>
+
+
+    </div>
+</template>
+
+<script>
+    import draggable from 'vuedraggable'
+
+    export default {
+        components: {
+            draggable,
+        },
+        data() {
+            return {
+                originItems: []
+            }
+        },
+        mounted() {
+            for (let i = 0; i < 10; i++) {
+                this.originItems.push({
+                    name: '元素' + i
+                })
+            }
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Helvetica, sans-serif;
+    }
+
+    html, body {
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+    }
+</style>
