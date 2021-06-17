6f6f2df3783fd4d79df2049dfad8967935e6c0c1
chenzw <656469722@qq.com>
2021-06-17 11:45:26 +0800

    update

---
+++ vue-dependencies-samples/vue-awesome/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="app">
+        <va-icon name="beer" pulse="true"></va-icon>
+        <va-icon name="bath" spin="true"></va-icon>
+
+        <!--
+            name: string  => 图标的名称。
+            scale: number|string => 用于调整图标的大小。默认为1.
+            spin: boolean => 用于指定图标是否旋转。默认为false. （不能与 一起使用pulse。）
+            pulse: boolean => 将脉冲效果设置为图标。默认为false. （不能与 一起使用spin。）
+            inverse: boolean => 如果设置为true，图标的颜色将变为#fff。默认为false.
+            flip: 'vertical'|'horizontal'|'both'  => 用于翻转图标。
+            label: string  => 设置aria-label如果规定的图标。
+            title: string  => 设置图标的标题。
+        -->
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        mounted() {
+
+        },
+        methods: {
+            startHacking() {
+                this.$notify({
+                    title: 'It works!',
+                    type: 'success',
+                    message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+                    duration: 5000
+                })
+            }
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Helvetica, sans-serif;
+    }
+
+    html, body {
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+    }
+</style>
---
+++ vue-dependencies-samples/vue-awesome/src/main.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App.vue'
+
+Vue.use(ElementUI)
+
+// 按需引入
+import 'vue-awesome/icons/beer'
+import 'vue-awesome/icons/bath'
+
+// 注册组件
+import Icon from 'vue-awesome/components/Icon'
+
+// 自定义默认大小
+Icon.register({
+  flag: {
+    width: 24,
+    height: 24,
+    raw:
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-flag"><path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path><line x1="4" y1="22" x2="4" y2="15"></line></svg>'
+  },
+  copy: {
+    width: 24,
+    height: 24,
+    raw:
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-copy"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>'
+  }
+});
+
+Vue.component('va-icon',Icon)
+
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
