15d57bbd3df43fd7e52a6a934a77c97b2ccd4042
chenzw <656469722@qq.com>
2021-07-02 15:40:28 +0800

    update

--- vue-dependencies-samples/template/src/main.js
+++ vue-dependencies-samples/template/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import routes from './routes'
 
 Vue.use(ElementUI)
 
 
 new Vue({
   el: '#app',
+  router: routes,
   render: h => h(App)
 })
