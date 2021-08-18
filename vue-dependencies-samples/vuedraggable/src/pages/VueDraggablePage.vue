c1e66ae0e856058a467df0e70a057f1cfed2907b
chenzw <656469722@qq.com>
2021-08-18 10:28:06 +0800

    update

--- vue-dependencies-samples/vuedraggable/src/pages/VueDraggablePage.vue
+++ vue-dependencies-samples/vuedraggable/src/pages/VueDraggablePage.vue
@@ -10,11 +10,13 @@
             </draggable>
         </el-card>
 
+        <br/>
 
-        <el-card header="多组拖拽">
+        <el-card header="拖拽动画">
             <draggable v-model="originItems">
+                <!-- 注意：此处key不能为index -->
                 <transition-group type="transition" name="flip-list">
-                    <el-button v-for="(item, index) in originItems" :key="index">
+                    <el-button v-for="(item, index) in originItems" :key="item.order">
                         {{ item.name }}
                     </el-button>
                 </transition-group>
@@ -33,8 +35,72 @@
                     </div>
                 </transition-group>
             </draggable>
+        </el-card>
+
+        <br/>
+
+        <el-card header="多组拖拽">
+            <el-row>
+                <el-col :span="4">
+                    <draggable v-model="originItems" animation="600" group="group1">
+                        <transition-group>
+                            <div v-for="(item, index) in originItems" :key="index"
+                                 :style="{ margin: '5px' }">
+                                <el-button>
+                                    {{ item.name }}
+                                </el-button>
+                            </div>
+                        </transition-group>
+                    </draggable>
+                </el-col>
+                <el-col :span="4">
+                    <draggable v-model="targetItems" animation="600" group="group1">
+                        <transition-group>
+                            <div v-for="(item, index) in targetItems" :key="item.order"
+                                 :style="{ margin: '5px'}">
+                                <el-button>
+                                    {{ item.name }}
+                                </el-button>
+                            </div>
+                        </transition-group>
+                    </draggable>
+                </el-col>
+            </el-row>
 
             <br/>
+
+            预占位 与 拷贝拖拽（不移除）
+            <el-row>
+                <el-col :span="4">
+                    <!-- 使用 pull:clone 来实现拷贝拖拽, put:false表示不能被放置 -->
+                    <draggable v-model="originItems" animation="600"
+                               :options="{group:{name: 'group2',pull:'clone',put: false}}" :sort="false"
+                               :clone="cloneComponent"
+                               @end="onEnd"
+                    >
+                        <transition-group>
+                            <div v-for="(item, index) in originItems" :key="index"
+                                 :style="{ margin: '5px' }">
+                                <el-button>
+                                    {{ item.name }}
+                                </el-button>
+                            </div>
+                        </transition-group>
+                    </draggable>
+                </el-col>
+                <el-col :span="4">
+                    <draggable v-model="targetItems" animation="600" group="group2">
+                        <transition-group>
+                            <div v-for="(item, index) in targetItems" :key="item.order"
+                                 :style="{ margin: '5px'}">
+                                <el-button>
+                                    {{ item.name }}
+                                </el-button>
+                            </div>
+                        </transition-group>
+                    </draggable>
+                </el-col>
+            </el-row>
 
         </el-card>
     </div>
@@ -62,6 +128,7 @@
         data() {
             return {
                 originItems: [],
+                targetItems: [],
                 list: message.map((name, index) => {
                     return {name, order: index + 1};
                 })
@@ -69,15 +136,32 @@
             }
         },
         mounted() {
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < 5; i++) {
                 this.originItems.push({
-                    name: '元素' + i,
+                    name: '源元素' + i,
                     order: i
-                })
-            }
+                });
 
+                this.targetItems.push({
+                    name: '目标元素' + i,
+                    order: i + 10
+                });
+            }
         },
-        methods: {},
+        methods: {
+            cloneComponent(origin) {
+                // 可以在此处自定义元素属性
+                return origin;
+            },
+            onEnd(e) {
+                console.log(e);
+
+                // 拖拽结束事件
+                if (e.from !== e.to) {
+
+                }
+            }
+        },
         computed: {}
     }
 </script>
