dfc2ff43a10f46606528a62eca94c0858fd84e34
chenzw <656469722@qq.com>
2021-10-13 11:07:28 +0800

    update

--- vue-dependencies-samples/record-rtc/src/pages/Home.vue
+++ vue-dependencies-samples/record-rtc/src/pages/Home.vue
@@ -1,26 +1,40 @@
 <template>
     <div>
 
-        <video controls autoplay playsinline></video>
+        <video id="video" controls autoplay playsinline></video>
 
+        <img src=""/>
 
-        <el-button @click="startVideoRecord">开始录制</el-button>
-        <el-button @click="stopVideoRecord">停止录制</el-button>
+        <div id="testBox" style="width: 500px; height: 300px; border: 1px solid #ccc;">
+           <input></input>
+            <button>xxx</button>
+        </div>
+
+        <canvas id="background-canvas" style="position:absolute; top:-99999999px; left:-9999999999px;"></canvas>
+
+        <el-button @click="startVideoRecord">开始摄像头录制</el-button>
+        <el-button @click="stopVideoRecord">停止摄像头录制</el-button>
 
         <el-button @click="startScreenRecord">开始屏幕录制</el-button>
         <el-button @click="stopScreenRecord">停止屏幕录制</el-button>
 
+        <el-button @click="startGifRecord">开始Gif录制</el-button>
+        <el-button @click="stopGifRecord">停止Gif录制</el-button>
+
+
+        <el-button @click="startFormRecord">开始表单录制</el-button>
+        <el-button @click="stopFormRecord">停止表单录制</el-button>
     </div>
 </template>
 
 <script>
     import RecordRTC from 'recordrtc'
-
+    import html2canvas from 'html2canvas'
 
     function captureCamera(callback) {
-        navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(function(camera) {
+        navigator.mediaDevices.getUserMedia({audio: true, video: true}).then(function (camera) {
             callback(camera);
-        }).catch(function(error) {
+        }).catch(function (error) {
             alert('Unable to capture your camera. Please check console logs.');
             console.error(error);
         });
@@ -28,22 +42,26 @@
 
 
     function addStreamStopListener(stream, callback) {
-        stream.addEventListener('ended', function() {
+        stream.addEventListener('ended', function () {
             callback();
-            callback = function() {};
+            callback = function () {
+            };
         }, false);
-        stream.addEventListener('inactive', function() {
+        stream.addEventListener('inactive', function () {
             callback();
-            callback = function() {};
+            callback = function () {
+            };
         }, false);
-        stream.getTracks().forEach(function(track) {
-            track.addEventListener('ended', function() {
+        stream.getTracks().forEach(function (track) {
+            track.addEventListener('ended', function () {
                 callback();
-                callback = function() {};
+                callback = function () {
+                };
             }, false);
-            track.addEventListener('inactive', function() {
+            track.addEventListener('inactive', function () {
                 callback();
-                callback = function() {};
+                callback = function () {
+                };
             }, false);
         });
     }
@@ -61,11 +79,11 @@
 
         // above constraints are NOT supported YET
         // that's why overriding them
-     /*   displaymediastreamconstraints = {
-            video: true
-        };*/
+        /*   displaymediastreamconstraints = {
+               video: true
+           };*/
 
-        if(navigator.mediaDevices.getDisplayMedia) {
+        if (navigator.mediaDevices.getDisplayMedia) {
             navigator.mediaDevices.getDisplayMedia(displaymediastreamconstraints).then(success).catch(error);
         } else {
             navigator.getDisplayMedia(displaymediastreamconstraints).then(success).catch(error);
@@ -73,12 +91,12 @@
     }
 
     function captureScreen(callback) {
-        invokeGetDisplayMedia(function(screen) {
-            addStreamStopListener(screen, function() {
+        invokeGetDisplayMedia(function (screen) {
+            addStreamStopListener(screen, function () {
                 // document.getElementById('btn-stop-recording').click();
             });
             callback(screen);
-        }, function(error) {
+        }, function (error) {
             console.error(error);
             alert('Unable to capture your screen. Please check console logs.\n' + error);
         });
@@ -86,79 +104,159 @@
 
 
     function checkGetDisplayMediaSupport() {
-        if(!navigator.getDisplayMedia && !navigator.mediaDevices.getDisplayMedia) {
+        if (!navigator.getDisplayMedia && !navigator.mediaDevices.getDisplayMedia) {
             throw Error("xxx");
         }
     }
 
+    let isRecordingStarted = false;
+    let isStoppedRecording = false;
+
+    (function looper() {
+        if(!isRecordingStarted) {
+            return setTimeout(looper, 500);
+        }
+
+        const testBox = document.getElementById("testBox");
+        var canvas2d = document.getElementById('background-canvas');
+        var context = canvas2d.getContext('2d');
+
+        html2canvas(testBox).then(function(canvas) {
+            context.clearRect(0, 0, canvas2d.width, canvas2d.height);
+            context.drawImage(canvas, 0, 0, canvas2d.width, canvas2d.height);
+
+            if(isStoppedRecording) {
+                return;
+            }
+
+            requestAnimationFrame(looper);
+        });
+    })();
+
 
     export default {
         data() {
-          return {
-              recorder: null
-          }
+            return {
+                recorder: null
+            }
         },
         methods: {
             startVideoRecord() {
+                const _this = this;
                 var video = document.querySelector('video');
-                captureCamera(function(camera) {
+                captureCamera(function (camera) {
                     video.muted = true;
                     video.volume = 0;
                     video.srcObject = camera;
 
-                    this.recorder = RecordRTC(camera, {
+                    _this.recorder = RecordRTC(camera, {
                         type: 'video'
                     });
 
-                    this.recorder.startRecording();
+                    _this.recorder.startRecording();
 
                     // release camera on stopRecording
-                    this.recorder.camera = camera;
+                    _this.recorder.camera = camera;
                 });
             },
             stopVideoRecord() {
+                const _this = this;
                 this.recorder.stopRecording(function () {
                     video.src = video.srcObject = null;
                     video.muted = false;
                     video.volume = 1;
-                    video.src = URL.createObjectURL(this.recorder.getBlob());
+                    video.src = URL.createObjectURL(_this.recorder.getBlob());
 
-                    this.recorder.camera.stop();
-                    this.recorder.destroy();
-                    this.recorder = null;
+                    _this.recorder.camera.stop();
+                    _this.recorder.destroy();
+                    _this.recorder = null;
                 });
             },
 
 
             startScreenRecord() {
                 // 检查是否支持
-                checkGetDisplayMediaSupport();
-
+                // checkGetDisplayMediaSupport();
+                const _this = this;
                 var video = document.querySelector('video');
-
-                let recorder = this.recorder;
-                captureScreen(function(screen) {
+                captureScreen(function (screen) {
                     video.srcObject = screen;
 
-                    recorder = RecordRTC(screen, {
+                    _this.recorder = RecordRTC(screen, {
                         type: 'video'
                     });
 
-                    recorder.startRecording();
+                    _this.recorder.startRecording();
 
                     // release screen on stopRecording
-                    recorder.screen = screen;
+                    _this.recorder.screen = screen;
                 });
             },
             stopScreenRecord() {
-                let recorder = this.recorder;
+                const _this = this;
                 this.recorder.stopRecording(function () {
                     video.src = video.srcObject = null;
-                    video.src = URL.createObjectURL(recorder.getBlob());
+                    video.src = URL.createObjectURL(_this.recorder.getBlob());
 
-                    recorder.screen.stop();
+                    _this.recorder.screen.stop();
+                    _this.recorder.destroy();
+                    _this.recorder = null;
+                });
+            },
+            startGifRecord() {
+                const _this = this;
+                captureCamera(function(camera) {
+                    _this.recorder = RecordRTC(camera, {
+                        type: 'gif',
+                        frameRate: 1,
+                        quality: 10,
+                        width: 360,
+                        hidden: 240,
+                        onGifRecordingStarted: function() {
+                            //document.querySelector('h1').innerHTML = 'Gif recording started.';
+                        },
+                        onGifPreview: function(gifURL) {
+                            image.src = gifURL;
+                        }
+                    });
+
+                    _this.recorder.startRecording();
+
+                    // release camera on stopRecording
+                    _this.recorder.camera = camera;
+
+                   // document.getElementById('btn-stop-recording').disabled = false;
+                });
+            },
+            stopGifRecord() {
+                recorder.stopRecording(function () {
+                    image.src = URL.createObjectURL(recorder.getBlob());
+                    recorder.camera.stop();
                     recorder.destroy();
                     recorder = null;
+                });
+            },
+            startFormRecord() {
+                isRecordingStarted = true;
+
+                var canvas2d = document.getElementById('background-canvas');
+
+                var testBox = document.getElementById('testBox');
+                canvas2d.width = testBox.clientWidth;
+                canvas2d.height = testBox.clientHeight;
+
+                this.recorder = new RecordRTC(canvas2d, {
+                    type: 'canvas',
+                    disableLogs: true
+                });
+                this.recorder.startRecording();
+            },
+            stopFormRecord() {
+                const _this = this;
+                const video = document.getElementById("video");
+                this.recorder.stopRecording(function() {
+                    var blob = _this.recorder.getBlob();
+                    video.src = URL.createObjectURL(blob);
                 });
             }
         }
