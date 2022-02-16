496ab662158f1f6c3645890d23ca1ae60c47769d
chenzw <656469722@qq.com>
2022-02-16 16:34:07 +0800

    update

---
+++ vue-dependencies-samples/vue-qr/src/pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <vue-qr text="https://baidu.com"/>
+
+      <vue-qr text="https://baidu.com"
+              :margin="0"
+              colorDark="red"
+              colorLight="#fff"
+              :logoScale="0.3"
+              :size="300"></vue-qr>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+      components: {
+          VueQr: ()=> import('vue-qr')
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
