d1071ff5d925bed7cac9cc59d7d3ce4ad8baf927
chenzw <656469722@qq.com>
2022-03-09 11:47:09 +0800

    update

---
+++ vue-dependencies-samples/html2canvas/src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    Home
+
+    <el-button @click="handleConvert">点击</el-button>
+  </div>
+</template>
+
+<script>
+import html2canvas from 'html2canvas';
+
+export default {
+  name: "Home",
+  methods: {
+    handleConvert() {
+      const imgBase64 = html2canvas(document.body).then(function (canvas) {
+        document.body.appendChild(canvas);
+        return canvas.toDataURL("image/png");
+      });
+
+      console.log(imgBase64);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
