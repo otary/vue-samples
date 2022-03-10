34a35a9f7ae5caedd7c60f43f0a6b30d1514034f
chenzw <656469722@qq.com>
2022-03-10 16:55:05 +0800

    update

---
+++ vue-dependencies-samples/swiper/src/pages/Home.vue
@@ -0,0 +1,89 @@
+<template>
+  <div :style="{width: '100%', height: '300px'}">
+    <swiper ref="mySwiper" :options="swiperOptions">
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide>
+      <swiper-slide>Slide 5</swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import {Swiper, SwiperSlide, directive} from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  directives: {
+    swiper: directive
+  },
+  data() {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable :true // 分页器是否可点击
+        },
+        /*navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },*/
+       // slidesPerView: 'auto', // 设置slider容器能够同时显示的slides数量
+       // spaceBetween: 10,  // 在slide之间设置距离（单位px）
+        breakpoints: {  // 响应式（不同浏览器宽度，显示不同的slider个数）
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 40
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20
+          },
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          }
+        }
+      }
+    }
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.$swiper
+    }
+  },
+  mounted() {
+    console.log('Current Swiper instance object', this.swiper)
+    this.swiper.slideTo(3, 1000, false)
+  }
+}
+</script>
+
+<style scoped>
+.swiper-container {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  border: 1px solid #ccc;
+}
+
+.swiper {
+  --swiper-theme-color: #ff6600; /* 设置Swiper风格 */
+  --swiper-navigation-color: #00ff33; /* 单独设置按钮颜色 */
+  --swiper-navigation-size: 30px; /* 设置按钮大小 */
+}
+</style>
