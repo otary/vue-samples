5552d35e72fe9c16497c7fa4557eab84a267dfde
chenzw <656469722@qq.com>
2022-03-18 14:05:00 +0800

    update

---
+++ vue-dependencies-samples/mockjs/src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    import Mock from 'mockjs'
+    export default {
+        mounted() {
+            let data = Mock.mock({
+                'regexp1': /^[a-z][A-Z][0-9]$/
+            });
+            console.log(data);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
