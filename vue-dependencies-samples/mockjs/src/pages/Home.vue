d2103db7aa17c591418384e6273b16486ec0d85a
chenzw <656469722@qq.com>
2022-03-19 08:40:57 +0800

    update

--- vue-dependencies-samples/mockjs/src/pages/Home.vue
+++ vue-dependencies-samples/mockjs/src/pages/Home.vue
@@ -12,6 +12,27 @@
                 'regexp1': /^[a-z][A-Z][0-9]$/
             });
             console.log(data);
+
+
+            const RandExp = require('randexp');
+            const r1 = new RandExp(/hello+ (world|to you)/).gen();
+            console.log(r1);
+
+            // 不支持中文
+            const r2 = new RandExp(/[\u4e00-\u9fa5]/).gen();
+            console.log(r2)
+
+            const r3 = new RandExp(/[\u3040-\u309f]/).gen();
+            console.log(r3)
+
+            // 根据正则生成数据（支持中文）
+            const ReRegExp = require('reregexp').default;
+            const rr1 = new ReRegExp(/[\u4e00-\u9fa5]/);
+            console.log(rr1.build())
+
+            const rr2 = new ReRegExp(/[\u3040-\u309f]/);
+            console.log(rr2.build())
+
         }
     }
 </script>
--- vue-dependencies-samples/vuepress2/docs/.vuepress/config.js
+++ vue-dependencies-samples/vuepress2/docs/.vuepress/config.js
@@ -1,5 +1,55 @@
 module.exports = {
     base: '/',
+    lang: 'zh-CN',
     title: 'Hello VuePress',
-    description: 'Just playing around'
+    description: 'Just playing around',
+    themeConfig: {
+        navbar: [
+            {
+                text: '配置',
+                link: '/guide/',
+            },
+            {
+                text: '提出问题',
+                link: 'https://github.com/xiao-akatsuki/axios/issues'
+            },
+            {
+                text: '参与项目',
+                link: 'https://github.com/xiao-akatsuki/axios/pulls'
+            },
+            {
+                text: 'language',
+                children: [
+                    {
+                        text: '中文',
+                        link: '/',
+                    },
+                    {
+                        text: 'English',
+                        link: 'https://xiao-akatsuki.github.io/axios.org/',
+                    },
+                ]
+            },
+            {
+                text: '版本',
+                children: [
+                    {
+                        text: '1.0.0',
+                        link: '/',
+                    },
+                ]
+            }
+        ],
+        sidebar: [
+            '/guide/README.md',
+            '/guide/MinimalExample.md',
+            '/guide/POSTRequests.md'
+        ],
+        sidebarDepth: 2,
+        logo: 'https://xiao-akatsuki.github.io/axios.org/logo/logo.svg',
+        repoLabel: 'axios',
+        repo: 'xiao-akatsuki/axios',
+        editLink: false,
+        darkMode: false,
+    },
 }
