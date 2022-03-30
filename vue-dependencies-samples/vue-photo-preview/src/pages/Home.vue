e9e73bc83d491d86fdc649ac41ff8796eb1e5129
chenzw <656469722@qq.com>
2022-03-30 11:51:02 +0800

    update

---
+++ vue-dependencies-samples/vue-photo-preview/src/pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="item2">
+            <img src="https://img0.baidu.com/it/u=1596977024,2747475854&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=421" preview="2" preview-text="描述文字">
+            <img src="https://img2.baidu.com/it/u=3496645542,2977113931&fm=253&fmt=auto&app=138&f=JPEG?w=749&h=500" preview="2" preview-text="描述文字2">
+        </div>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import preview from 'vue-photo-preview';
+    import 'vue-photo-preview/dist/skin.css';
+    Vue.use(preview);
+
+    export default {
+        mounted() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
