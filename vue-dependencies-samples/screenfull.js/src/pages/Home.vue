5f680eb49fd0f1e3b8b721ca4382dd3da4cb3bae
chenzw <656469722@qq.com>
2022-03-31 21:02:12 +0800

    update

---
+++ vue-dependencies-samples/screenfull.js/src/pages/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <el-button @click="handleFullScreen" value="全屏">全屏</el-button>
+  </div>
+</template>
+
+<script>
+import screenfull from 'screenfull';
+
+export default {
+  name: "Home",
+  mounted() {
+
+  },
+  methods: {
+    handleFullScreen() {
+      if (screenfull.isEnabled) {
+        // 全屏
+        // screenfull.request();
+
+        // 退出全屏
+        //screenfull.exit();
+
+        // 切换全屏
+        screenfull.toggle();
+
+        // 对单个元素全屏
+        // screenfull.request(element);
+      }
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
