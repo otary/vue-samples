5304aea000e94b0a19656b9162618d4cb5fa067e
chenzw <656469722@qq.com>
2022-08-05 14:50:56 +0800

    update

---
+++ vue-dependencies-samples/split.js/src/pages/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div>
+            <div id="block1">
+                内容块1
+            </div>
+            <div id="block2">
+                内容块2
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Split from 'split.js'
+
+    export default {
+        name: "Home",
+        mounted() {
+            Split(['#block1', '#block2'], {
+                sizes: [25, 75],
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    #block1 {
+        width: 200px;
+        border: 1px solid;
+    }
+
+    #block2 {
+        width: 200px;
+        border: 1px solid;
+    }
+</style>
