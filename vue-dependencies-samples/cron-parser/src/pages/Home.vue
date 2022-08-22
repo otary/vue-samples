8dcd77c2b8df5a7c4d93e63affe359ad988e35b4
chenzw <656469722@qq.com>
2022-08-22 17:18:26 +0800

    update

---
+++ vue-dependencies-samples/cron-parser/src/pages/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        Home
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        mounted() {
+            var parser = require('cron-parser');
+            var interval = parser.parseExpression('*/2 * * * *');
+            console.log('Date: ', interval.next().toString());
+
+
+            var interval = parser.parseExpression('0 */12 * * *');
+            console.log('Date: ', interval.next().toString());
+
+            var interval = parser.parseExpression('0 0 18 28-31 * ?');
+            console.log('Date: ', interval.next().toString());
+
+
+            var interval = parser.parseExpression('0 0 18 L * ?');
+            console.log('Date: ', interval.next().toString());
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
