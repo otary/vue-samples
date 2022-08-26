1bbed07eaca6542eb170a549e405cea403c4b483
chenzw <656469722@qq.com>
2022-08-26 09:10:07 +0800

    update

---
+++ vue-dependencies-samples/fonteditor/src/pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        Home
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            const Font = require('fonteditor-core').Font;
+            let buffer = null;
+            let font = Font.create(buffer, {
+                type: 'ttf', // support ttf, woff, woff2, eot, otf, svg
+                subset: [65, 66], // only read `a`, `b` glyf
+                hinting: true, // save font hinting
+                compound2simple: true, // transform ttf compound glyf to simple
+                inflate: null, // inflate function for woff
+                combinePath: false, // for svg path
+            });
+            let fontObject = font.get();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
